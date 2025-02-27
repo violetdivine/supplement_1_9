727b516a0c62757369eb5759ee2912c35aca3686
[name] <[email address]>
2025-02-27 18:57:55 -0500

    Code: Implemented Quarter that validates the values, quarter equality, and overrides the operators.

--- supplement_1_9/supplement_1_9.cs
+++ supplement_1_9/supplement_1_9.cs
@@ -47,4 +47,46 @@ public class RandomFloatEnumerable : IEnumerable<double>{
 
         return GetEnumerator();
     }
+
+    public class Quarter{
+
+        public double Value { get; }
+
+        public Quarter(double value){
+
+            if (value < 0.0 || value >= 1.0){
+
+                throw new ArgumentOutOfRangeException(nameof(value), "Value must be in range 0.0-1.0");
+            Value = value;
+            }
+        }
+
+        private int GetQuarter(){
+            
+            if (Value < 0.25) return 1;
+            if (Value < 0.5) return 2;
+            if (Value < 0.75) return 3;
+            return 4;
+        }
+
+        public static bool operator ==(Quarter q1, Quarter q2) => q1.GetQuarter() == q2.GetQuarter();
+        public static bool operator !=(Quarter q1, Quarter q2) => !(q1 == q2);
+
+        public static bool operator >(Quarter q1, Quarter q2) => q1.Value > q2.Value;
+        public static bool operator <(Quarter q1, Quarter q2) => q1.Value < q2.Value;
+
+        public static bool operator >=(Quarter q1, Quarter q2) => q1.Value >= q2.Value;
+        public static bool operator <=(Quarter q1, Quarter q2) => q1.Value <= q2.Value;
+
+        public override bool Equals(object obj)
+        {
+            return obj is Quarter quarter && this == quarter;
+        }
+
+        public override int GetHashCode()
+        {
+            return GetQuarter().GetHashCode();
+        }
+
+    }
 }
