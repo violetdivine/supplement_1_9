75c7a523c750da544aaa13309358310d2e860e0c
[name] <[email address]>
2025-02-27 18:15:52 -0500

    Code: RandomFloatEnumerable class generates random numbers between 0.0 and 1.0 and throws the exception when necessary.

--- supplement_1_9/supplement_1_9.cs
+++ supplement_1_9/supplement_1_9.cs
@@ -1,4 +1,6 @@
-﻿namespace supplement_1_9;
+﻿using System.Collections;
+
+namespace supplement_1_9;
 
 /// <summary>
 /// Throws an exception when an invalid sequence is recognized.
@@ -11,4 +13,38 @@ public class InvalidSequenceException : Exception{
     /// <param name="message">The error message.</param>
     public InvalidSequenceException(string message) : base(message) {}
 
+}
+
+public class RandomFloatEnumerable : IEnumerable<double>{
+
+    private Random _random = new Random();
+
+    public IEnumerator<double> GetEnumerator(){
+
+        int lowCount = 0;
+
+        while (true){
+
+            double num = _random.NextDouble();
+            yield return num;
+
+            if(num <= 0.5){
+
+                lowCount++;
+                if (lowCount == 3){
+                    throw new InvalidSequenceException("3 consecutive numbers are " +
+                    "less than or equal to 0.5.");
+                }
+            }
+            else{
+                
+                lowCount = 0;
+            }
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator(){
+
+        return GetEnumerator();
+    }
 }
