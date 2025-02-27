8f23cf4f1e2d5d99180a699c17bea41a295d167a
[name] <[email address]>
2025-02-27 17:33:16 -0500

    initial commit

---
+++ supplement_1_9.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace supplement_1_9.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ supplement_1_9/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace supplement_1_9;
+
+public class Class1
+{
+
+}
