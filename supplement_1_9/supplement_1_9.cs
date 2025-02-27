f03c5b16d2699b94bc16a22ad3b5d3539d0219eb
[name] <[email address]>
2025-02-27 17:53:33 -0500

    Doc: Created documentation for entire InvalidSequenceException class.

--- supplement_1_9/supplement_1_9.cs
+++ supplement_1_9/supplement_1_9.cs
@@ -1,7 +1,14 @@
 ﻿namespace supplement_1_9;
 
+/// <summary>
+/// Throws an exception when an invalid sequence is recognized.
+/// </summary>
 public class InvalidSequenceException : Exception{
     
+    /// <summary>
+    /// Initializes InvalidSequenceException() with a error message.
+    /// </summary>
+    /// <param name="message">The error message.</param>
     public InvalidSequenceException(string message) : base(message) {}
 
 }
