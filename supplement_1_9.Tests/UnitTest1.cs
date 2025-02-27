c51d6eb4b519fe6c19d2a7dcc4cd9f09cfd4a44b
[name] <[email address]>
2025-02-27 17:59:45 -0500

    Chore: Created test that will verify if numbers are within the correct range and implemented a for loop to prevent an infinite loop.

--- supplement_1_9.Tests/UnitTest1.cs
+++ supplement_1_9.Tests/UnitTest1.cs
@@ -11,4 +11,20 @@ public class UnitTest1
         var ex = new InvalidSequenceException("Error message - Test");
         Assert.Equal("Error message - Test", ex.Message);
     }
+    
+    [Fact]
+    public void ShouldGenerateCorrectNumbersInRange()
+    {
+        var generator = new RandomFloatEnumerable();
+        var enumerator = generator.GetEnumerator();
+
+        for (int i=0; i < 10; i++){
+
+            enumerator.MoveNext();
+            double value = enumerator.Current;
+            
+            Assert.InRange(value, 0.0, 1.0);
+        }
+    }
+
 }
